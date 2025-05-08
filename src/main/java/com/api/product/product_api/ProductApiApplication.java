d0f2dbbc34ada1af821b05793513fd65dc045278
Gabriel <gabrieldacruzbogo01@gmail.com>
2025-05-08 18:49:36 -0300

    teste release

--- src/main/java/com/api/product/product_api/ProductApiApplication.java
+++ src/main/java/com/api/product/product_api/ProductApiApplication.java
@@ -3,6 +3,8 @@ package com.api.product.product_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//teste
+
 @SpringBootApplication
 public class ProductApiApplication {
 
