f8e63e97aa37dca5bbe3a11718aa5f4fd5f647c6
Gabriel <gabrieldacruzbogo01@gmail.com>
2025-05-10 15:39:27 -0300

    testdw21qdasqwdqwddqwdqwdqwdwqdwqdwqdqwdqwdqw

--- src/main/java/com/api/product/product_api/controller/ProductController.java
+++ src/main/java/com/api/product/product_api/controller/ProductController.java
@@ -38,7 +38,7 @@ public class ProductController {
     }
 
  
-    //teste3dwqdqwdwqdqwdwqdwqdwqdqwdqwdqwdqwdqwdqwdasdasdqwwdqdqwdqwdwqdwqqdwqwddqwdwqdsadasdasdasdasdasdas
+    //teste3dwqdqwdwqdqwdwqdwqdwqdqwdqwdqwdqwdqwdqwdasdasdqwwdqdqwdqwdwqdwqqdwqwddqwdwqdsadasdasdasdasddasdasdasdasdasadsasdas
     @GetMapping("/health")
     public ResponseEntity<String> checkHealth() {
         return ResponseEntity.ok("API funcionando corretamente.");
