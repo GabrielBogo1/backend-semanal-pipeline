812236a081a478bca5a28d64bed09f51a73e4bdc
Gabriel <gabrieldacruzbogo01@gmail.com>
2025-05-10 20:49:35 -0300

    teste d

--- src/main/java/com/api/product/product_api/controller/ProductController.java
+++ src/main/java/com/api/product/product_api/controller/ProductController.java
@@ -54,7 +54,7 @@ public class ProductController {
 
     //@GetMapping
     //public ResponseEntity<List<Product>> listAll() {
-      //  return ResponseEntity.ok(this.productRepository.findAll());dwqdqwdqwdwqdwqdwqdwq
+      //  return ResponseEntity.ok(this.productRepository.findAll());dwqdqwdqwdwqdwqdwqdwqdwqdqw
     //}
 
     @PostMapping
