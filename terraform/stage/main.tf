cd558d08df2428ef8b31496cef88ab130e94dad6
Gabriel <gabrieldacruzbogo01@gmail.com>
2025-06-14 20:50:04 -0300

    teste

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -15,23 +15,6 @@ provider "google" {
   credentials = file("account.json")
 }
 
-resource "google_container_cluster" "primary" {
-  name     = "gke-stage"
-  location = "us-central1-a"  # Zona exata para criar um cluster ZONAL
-
-  remove_default_node_pool = true
-  initial_node_count       = 1
-
-  enable_legacy_abac = true
-
-  master_authorized_networks_config {
-    cidr_blocks {
-      cidr_block   = "0.0.0.0/0"
-      display_name = "All networks"
-    }
-  }
-}
-
 resource "google_container_node_pool" "primary_nodes" {
   name       = "primary-node-pool"
   location   = "us-central1-a"
